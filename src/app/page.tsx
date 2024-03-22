23567ecc8524170c7704b199d1465f65cefce948
Oluwaseyi <oluseyitimilehin3@gmail.com>
2024-03-22 14:36:19 -0700

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,17 @@
 
 import logo  from "@/assets/globus.png"
 import {Navbar, Hero, Features, Forging, Empowerment, Questions} from "@/Components"
-import React, { useState } from 'react';
 
-import Link from "next/link"
-import { HiMenu } from "react-icons/hi";
-import { AiFillCloseCircle } from "react-icons/ai";
-import { BrowserRouter } from "react-router-dom";
+
+
+
 
 export default function Page(){
-  const [toggleSidebar, setToggleSidebar] = useState(false);
-
-  const toggleNav = () => {
-    setToggleSidebar(!toggleSidebar);
-  };
+  
 
   return (
     <main>
-      <BrowserRouter>
+      
       
       
       <Navbar/>
@@ -28,7 +22,7 @@ export default function Page(){
           <Forging/>
           <Empowerment/>
           <Questions/>
-          </BrowserRouter>
+        
         
     </main>
   )
